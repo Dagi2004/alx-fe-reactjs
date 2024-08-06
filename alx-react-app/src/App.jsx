aa60cc77db566d3417be91e7736163e58b50c6e5
Dagi <dagmawimilkias@gmail.com>
2024-08-06 19:33:13 +0300

    Modified App.js

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import WelcomeMessage from "./WelcomeMessage";
+import WelcomeMessage from "./components/WelcomeMessage";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <WelcomeMessage>
+    <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,7 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </WelcomeMessage>
+      <WelcomeMessage />
+    </>
   );
 }
 

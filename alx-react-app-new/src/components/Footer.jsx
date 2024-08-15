787bd0811b486c6683b948712881ef35a1dadb41
Dagi <dagmawimilkias@gmail.com>
2024-08-15 20:16:21 +0300

    Added alx-new-react

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div>
+      <footer style={{ backgroundColor: "red", fontSize: "25px" }}>
+        <p>© 2023 City Lovers</p>
+      </footer>
+    </div>
+  );
+}
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,18 @@
+function MainContent() {
+  return (
+    <div
+      style={{
+        padding: "20px",
+        color: "white",
+        backgroundColor: "blueviolet",
+        borderRadius: "10px",
+        border: "1px solid black",
+      }}
+    >
+      <main>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+      </main>
+    </div>
+  );
+}
+export default MainContent;

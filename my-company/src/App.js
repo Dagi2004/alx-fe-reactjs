07589473b7c179472a75b4df736e2b968c243652
Dagi <dagmawimilkias@gmail.com>
2024-08-15 20:33:31 +0300

    My-company

---
+++ my-company/src/App.js
@@ -0,0 +1,22 @@
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import Navbar from "./Navbar";
+
+import { Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ my-company/src/pages/Services.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Services() {
+  return (
+    <div
+      style={{
+        padding: "20px",
+        backgroundColor: "lightblue",
+        textAlign: "center",
+      }}
+    >
+      <h1>Our Services</h1>
+      <ul style={{ listStyle: "none" }}>
+        <li>Technology Consulting</li>
+        <li>Market Analysis</li>
+        <li>Product Development</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;

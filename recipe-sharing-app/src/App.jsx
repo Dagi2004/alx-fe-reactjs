80287e2336c2d293447a4416795bf3e3e96c42c9
Dagi <dagmawimilkias@gmail.com>
2024-08-25 11:38:23 +0300

    recipe sharing

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,29 @@
 import "./App.css";
 import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 import AddRecipeForm from "./components/AddRecipeForm";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const RecipePage = ({ match }) => {
+  const { id } = match.params;
+  return <RecipeDetails recipeId={id} />;
+};
 
 function App() {
   return (
-    <>
-      <RecipeList />
-      <AddRecipeForm />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipePage />} />
+      </Routes>
+    </Router>
   );
 }
+const HomePage = () => (
+  <>
+    <RecipeList />
+    <AddRecipeForm />
+  </>
+);
 
 export default App;
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,16 +1,24 @@
+import { Link } from "react-router-dom";
 import useRecipeStore from "../components/recipeStore";
+import DeleteRecipeButton from "../components/DeleteRecipeButton";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <Link to={`/recipe/${recipe.id}`}>{recipe.title}</Link>
+            <div>
+              <h3>{recipe.title}</h3>
+              <p>{recipe.description}</p>
+              <DeleteRecipeButton recipeId={recipe.id} />
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };

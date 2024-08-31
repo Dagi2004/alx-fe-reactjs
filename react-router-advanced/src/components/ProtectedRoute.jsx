5440dc12e2852d82757b38af7f8011cb97ed53fc
Dagi <dagmawimilkias@gmail.com>
2024-08-31 12:08:29 +0300

    Advacned Routing

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,9 +1,12 @@
 import { Navigate } from "react-router-dom";
 
-// Simulate authentication status
-const isAuthenticated = true; // Change to false to test unauthenticated access
+const useAuth = () => {
+  const isAuthenticated = true; // Change to false to test unauthenticated access
+  return { isAuthenticated };
+};
 
 const ProtectedRoute = ({ element }) => {
+  const { isAuthenticated } = useAuth();
   return isAuthenticated ? element : <Navigate to="/profile" />;
 };
 

800c5bba1a3a54c943b4ccea3366ab7934827a95
Dagi <dagmawimilkias@gmail.com>
2024-08-31 12:05:48 +0300

    Advacned Routing

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -26,7 +26,7 @@ function App() {
           <Route path="details" element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
-        <Route path="/blog/:postId" element={<BlogPost />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import { useParams } from "react-router-dom";
 
 const BlogPost = () => {
-  const { postId } = useParams();
+  const { id } = useParams();
 
   return (
     <div>
-      <h2>Blog Post {postId}</h2>
+      <h2>Blog Post {id}</h2>
       {/* Fetch and display blog post content based on postId */}
     </div>
   );

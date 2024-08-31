148b9b44cb45687c3be3b265eed0324a502fa2b2
Dagi <dagmawimilkias@gmail.com>
2024-08-31 11:51:28 +0300

    Advacned Routing

--- react-router-advanced/src/components/Profile.jsx
+++ react-router-advanced/src/components/Profile.jsx
@@ -1,12 +1,19 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Routes, Route, Outlet } from "react-router-dom";
+import ProfileDetails from "./ProfileDetails";
+import ProfileSettings from "./ProfileSettings";
 
 const Profile = () => {
   return (
     <div>
+      <h2>Profile</h2>
       <nav>
-        <Link to="details">ProfileDetails</Link> |{" "}
-        <Link to="settings">ProfileSettings</Link>
+        <Link to="details">Profile Details</Link> |{" "}
+        <Link to="settings">Profile Settings</Link>
       </nav>
+      <Routes>
+        <Route path="details" element={<ProfileDetails />} />
+        <Route path="settings" element={<ProfileSettings />} />
+      </Routes>
       <Outlet />
     </div>
   );

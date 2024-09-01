7f0c061739b4e121f21ae33dee25d6d8fd97041d
Dagi <dagmawimilkias@gmail.com>
2024-09-01 17:50:41 +0300

    form handling

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import RegistrationForm from "./components/RegistrationForm";
 import FormikForm from "./components/FormikForm";
+import RegistrationForm from "./components/RegistrationForm";
 function App() {
   return (
     <>
-      <h1>User Registration Using Controlled Element</h1>
-      <RegistrationForm />
-      <br></br>
-      <h1>Userr Registration Using Formik</h1>
+      <RegistrationForm />.
       <FormikForm />
     </>
   );

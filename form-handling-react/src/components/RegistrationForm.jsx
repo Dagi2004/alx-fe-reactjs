df28e16091f608ab356587750480564fcc0f1e81
Dagi <dagmawimilkias@gmail.com>
2024-09-01 17:35:04 +0300

    Handle form using contolled element

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,87 +1,77 @@
-import React, { useState } from "react";
-
+import { useState } from "react";
 const RegistrationForm = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    email: "",
-    password: "",
-  });
-
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
+  const error = {};
   const handleSubmit = (e) => {
     e.preventDefault();
-    const validationErrors = {};
-
-    if (!formData.username.trim()) {
-      validationErrors.username = "Username is required";
+    if (!username) {
+      errors.username(`Username must be at least 3 characters long.`);
+      return;
     }
-
-    if (!formData.email.trim()) {
-      validationErrors.email = "Email is required";
+    if (!password) {
+      errors.password("Password must be at least 8 characters");
+      return;
     }
-
-    if (!formData.password.trim()) {
-      validationErrors.password = "Password is required";
-    } else if (formData.password.length < 6) {
-      validationErrors.password = "Password must be at least 6 characters";
+    if (!email) {
+      errors.email("Invalid email");
+      return;
     }
+    setErrors(error);
+    resetForm();
 
-    setErrors(validationErrors);
-
-    if (Object.keys(validationErrors).length === 0) {
-      // Handle form submission if no errors
-      alert("Form submitted:", formData);
-    }
+    console.log("Username:", username);
+    console.log("Email:", email);
+    console.log("Password:", password);
+  };
+  const resetForm = () => {
+    setUsername("");
+    setEmail("");
+    setPassword("");
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-        {errors.username && (
-          <span style={{ color: "red" }}>{errors.username}</span>
-        )}
-      </div>
-      <div>
-        <input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-        />
-        {errors.email && <span style={{ color: "red" }}>{errors.email}</span>}
-      </div>
-      <div>
-        <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        {errors.password && (
-          <span style={{ color: "red" }}>{errors.password}</span>
-        )}
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <h1>Components Form</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            value={username}
+            placeholder="Enter your Username"
+            required
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            value={email}
+            placeholder="Enter your email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            value={password}
+            placeholder="Enter your Password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </>
   );
 };
-
 export default RegistrationForm;

ad12c09fbedaab0d1be09596a8a0c2a9e3df6a25
Dagi <dagmawimilkias@gmail.com>
2024-09-10 12:38:01 +0300

    use Link to navigate to different recipes

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 const HomePage = () => {
   const [recipes, setRecipes] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("/data.json")
@@ -15,23 +14,24 @@ const HomePage = () => {
       <h1 className="text-3xl font-bold mb-5 mt-5 text-center">Recipe List</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {recipes.map((recipe) => (
-          <div
-            key={recipe.id}
-            className="overflow-hidden rounded-lg border border-gray-100 bg-white  shadow-md hover:shadow-lg transition-shadow"
-            onClick={() => navigate(`/recipe/${recipe.id}`)}
-          >
-            <img
-              src={recipe.imageURL}
-              alt={recipe.title}
-              className="h-56 w-full object-cover"
-            />
-            <h2 className="text-xl font-semibold mt-2 text-center">
-              {recipe.title}
-            </h2>
-            <p className="text-gray-600 px-4 max-wd-md text-center ">
-              {recipe.summary}
-            </p>
-          </div>
+          <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
+            <div
+              key={recipe.id}
+              className="overflow-hidden rounded-lg border border-gray-100 bg-white  shadow-md hover:shadow-lg transition-shadow"
+            >
+              <img
+                src={recipe.imageURL}
+                alt={recipe.title}
+                className="h-56 w-full object-cover"
+              />
+              <h2 className="text-xl font-semibold mt-2 text-center">
+                {recipe.title}
+              </h2>
+              <p className="text-gray-600 px-4 max-wd-md text-center ">
+                {recipe.summary}
+              </p>
+            </div>
+          </Link>
         ))}
       </div>
     </div>
